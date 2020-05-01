740e7a28af9dd0fde25434f201c9b6ef1856dd0f
Willem Dendauw <willem.dendauw@ugent.be>
2020-05-01 10:25:33 +0200

    verder werken aan labo 1: quiz van mobiele toepassingen

--- Mobieleapps/Quiz/app/src/main/java/com/example/quiz/MainActivity.kt
+++ Mobieleapps/Quiz/app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.quiz.databinding.ActivityMainBinding
+import com.example.quiz.model.QuizMaster
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.quizMaster = QuizMaster
+        binding.handler = this
+    }
+
+    fun onHintClick(){
+        val toast = Toast.makeText(applicationContext, "test", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun onOkClick(){
+
     }
 }
